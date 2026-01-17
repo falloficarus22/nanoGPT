22dbcb0c3424646b92c57ee70e9df45ee349e4f6
Abhishek <norizzabhii@gmail.com>
2026-01-17 07:10:09 +0000

    Added attention head to the model

--- model.py
+++ model.py
@@ -2,6 +2,34 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class AttentionHead(nn.module):
+    """Single attention head"""
+
+    def __init__(self, n_embd, block_size):
+        super.__init__()
+        self.key = torch.Linear(n_embd, n_embd, bias = False)
+        self.query = torch.Linear(n_embd, n_embd, bias = False)
+        self.value = torch.Linear(n_embd, n_embd, bias = False)
+
+        self.register_buffer(
+            'tril', torch.tril(torch.ones(block_size, block_size))
+        )
+
+    def forward(self, x):
+        B, T, C = x.shape
+
+        K = self.key(x)
+        Q = self.query(x)
+        V = self.value(x)
+
+        attn = (Q @ K.transpose(-1, -2)) / (C ** 0.5)
+        attn = attn.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        weights = F.softmax(attn, dim=-1)
+        out = weights @ V
+
+        return out
+
+
 class LanguageModel(nn.Module):
 
     def __init__(self, vocab_size, n_embd, block_size):
@@ -10,13 +38,14 @@ class LanguageModel(nn.Module):
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         self.block_size = block_size
+        self.attn_head = Head(n_embd, block_size)
 
     def forward(self, idx, targets = None):
         B, T = idx.shape
         tok_embd = self.token_embedding_table(idx)
         pos_embd = self.position_embedding_table(torch.arange(T))
         x = tok_embd + pos_embd
-
+        x = self.attn_head(x)
         logits = self.lm_head(x)
 
         if targets is None:
